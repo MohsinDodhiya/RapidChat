8486bfbb9ab804cb5bdd041f01d7a9012dbb1df7
MohsinDodhiya <mdodhiya653@rku.ac.in>
2024-11-06 16:19:34 +0530

    cors error

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,10 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173", // Local development origin
+      "https://rapid-chat-olive.vercel.app", // Deployed frontend origin
+    ],
     methods: ["GET", "POST"],
   },
 });
